2e63c1ecd82b3f5272bec806cfc07fe3303eda8c
myluki2000 <lukas636@live.com>
2019-02-24 19:29:22 +0100

    Barebones screen handling structure

---
+++ PeridotEngine/UI/Screen.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PeridotEngine.UI
+{
+    class Screen
+    {
+
+
+        public void Initialize() { }
+
+        public void Update(GameTime gameTime) { }
+
+        public void Draw(SpriteBatch sb)
+        {
+            
+        }
+    }
+}
---
+++ PeridotEngine/UI/ScreenHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PeridotEngine.UI
+{
+    static class ScreenHandler
+    {
+        private static Screen selectedScreen;
+        internal static Screen SelectedScreen
+        {
+            get => selectedScreen;
+
+            set
+            {
+                selectedScreen = value;
+                
+                // init screen
+                if(selectedScreen != null)
+                    selectedScreen.Initialize();
+            }
+        }
+
+        public static void Draw(SpriteBatch sb)
+        {
+            if (selectedScreen != null)
+                selectedScreen.Draw(sb);
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+            if (selectedScreen != null)
+                selectedScreen.Update(gameTime);
+        }
+    }
+}
