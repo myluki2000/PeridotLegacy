8479af5b635750d1d7aae835bb2d078188d3e48e
myluki2000 <lukas636@live.com>
2020-04-03 18:01:53 +0200

    Basic stuff for time stimulant.

--- PeridotEngine/Game/World/WorldObjects/Entities/StimulantsPlayer.cs
+++ PeridotEngine/Game/World/WorldObjects/Entities/StimulantsPlayer.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using PeridotEngine.Engine.World.WorldObjects.Entities;
 
 namespace PeridotEngine.Game.World.WorldObjects.Entities
 {
     class StimulantsPlayer : Player
     {
+        private uint timeStimulantsAmount;
 
+        private KeyboardState lastKeyboardState;
+
+        /// <inheritdoc />
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.D1) && lastKeyboardState.IsKeyUp(Keys.D2))
+            {
+
+            }
+
+            lastKeyboardState = keyboardState;
+        }
     }
 }
