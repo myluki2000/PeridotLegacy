cef9d5b06da6aa7a790bb980c20e3ba177fc2af0
myluki2000 <lukas636@live.com>
2020-04-03 17:29:00 +0200

    Added time stimulant texture data xml.

--- PeridotEngine/Game/UI/UIElements/StimulantUI.cs
+++ PeridotEngine/Game/UI/UIElements/StimulantUI.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using PeridotEngine.Engine.Graphics;
+using PeridotEngine.Engine.Resources;
 using PeridotEngine.Engine.UI.UIElements;
 
 namespace PeridotEngine.Game.UI.UIElements
 {
     class StimulantUI : UIElement
     {
+        private Sprite timeStimulantSprite;
+
+        public StimulantUI()
+        {
+            /*timeStimulantSprite = new Sprite(
+                TextureManager.LoadTexture("Content/UI/Stimulants/time"),
+                new Vector2(0, 0)
+            );*/
+        }
+
         /// <inheritdoc />
         public override void Draw(SpriteBatch sb)
         {
